ced843b6f5cd329863f7c21fc785ba6fcff5d63f
Chris <xryswelter@gmail.com>
2022-09-16 22:03:36 -0700

    laptop commit

--- src/app/core/interfaces/character-stats.interface.ts
+++ src/app/core/interfaces/character-stats.interface.ts
@@ -1,58 +1,67 @@
-interface characterStats {
+export interface CHARACTER_STATS {
   coreStats: {
-    name: string;
-    class: string;
-    level: number;
-    alignment: string;
+    name?: string;
+    class?: string;
+    level?: number;
+    alignment?: string;
   };
   baseStats: {
-    strength: number;
-    dexerity: number;
-    constitution: number;
-    intelligence: number;
-    wisdom: number;
-    charisma: number;
+    strength?: number;
+    dexterity?: number;
+    constitution?: number;
+    intelligence?: number;
+    wisdom?: number;
+    charisma?: number;
+  };
+  baseModifier: {
+    strength?: number;
+    dexterity?: number;
+    constitution?: number;
+    intelligence?: number;
+    wisdom?: number;
+    charisma?: number;
   };
   savingThrows: {
-    strength: { proficient?: boolean; savingValue: number };
-    dexerity: { proficient?: boolean; savingValue: number };
-    constitution: { proficient?: boolean; savingValue: number };
-    intelligence: { proficient?: boolean; savingValue: number };
-    wisdom: { proficient?: boolean; savingValue: number };
-    charisma: { proficient?: boolean; savingValue: number };
+    strength?: { proficient?: boolean; savingValue?: number };
+    dexterity?: { proficient?: boolean; savingValue?: number };
+    constitution?: { proficient?: boolean; savingValue?: number };
+    intelligence?: { proficient?: boolean; savingValue?: number };
+    wisdom?: { proficient?: boolean; savingValue?: number };
+    charisma?: { proficient?: boolean; savingValue?: number };
   };
   battleStats: {
-    currentHP: number;
-    maxHP: number;
-    temporaryHP: number;
-    armorClass: number;
-    generalDifficultClass: number;
-    abnormalStatus: {
-      blind: boolean;
-      charmed: boolean;
-      deafened: boolean;
-      frightened: boolean;
-      grappled: boolean;
-      incapacitated: boolean;
-      invisible: boolean;
-      paralyzed: boolean;
-      petrified: boolean;
-      poisoned: boolean;
-      prone: boolean;
-      restrained: boolean;
-      stunned: boolean;
-      unconscious: boolean;
-      exhausted: {
-        1: boolean;
-        2: boolean;
-        3: boolean;
-        4: boolean;
-        5: boolean;
-        6: boolean;
+    currentHP?: number;
+    maxHP?: number;
+    temporaryHP?: number;
+    hitdice?:number;
+    armorClass?: number;
+    generalDifficultClass?: number;
+    abnormalStatus?: {
+      blind?: boolean;
+      charmed?: boolean;
+      deafened?: boolean;
+      frightened?: boolean;
+      grappled?: boolean;
+      incapacitated?: boolean;
+      invisible?: boolean;
+      paralyzed?: boolean;
+      petrified?: boolean;
+      poisoned?: boolean;
+      prone?: boolean;
+      restrained?: boolean;
+      stunned?: boolean;
+      unconscious?: boolean;
+      exhausted?: {
+        1?: boolean;
+        2?: boolean;
+        3?: boolean;
+        4?: boolean;
+        5?: boolean;
+        6?: boolean;
       };
     };
     speed:{
-        runSpeed:number
+        runSpeed?:number
     }
   };
 }
--- src/app/core/ui/stat-block/stat-block.component.ts
+++ src/app/core/ui/stat-block/stat-block.component.ts
@@ -1,6 +1,21 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { PLAYER_SKILL_PROFICIENCY, RAW_STAT_BLOCK } from '../../interfaces/stat-block.interface';
-
+import { Component, Input } from '@angular/core';
+import { CHARACTER_STATS } from '../../interfaces/character-stats.interface';
+import { ModifierService } from '../../services/modifier.service';
+import {
+  artificer,
+  barbarian,
+  bard,
+  cleric,
+  druid,
+  fighter,
+  monk,
+  paladin,
+  ranger,
+  rogue,
+  sorcerer,
+  warlock,
+  wizard,
+} from '../../constants/class-stats.constants';
 
 @Component({
   selector: 'app-stat-block',
@@ -8,5 +23,56 @@ import { PLAYER_SKILL_PROFICIENCY, RAW_STAT_BLOCK } from '../../interfaces/stat-
   styleUrls: ['./stat-block.component.scss'],
 })
 export class StatBlockComponent {
+  constructor(private modifierService: ModifierService) {}
 
+  characterStats: CHARACTER_STATS;
+
+  @Input() name: string;
+  @Input() characterClass: string[];
+  @Input() classLevel: number[];
+  @Input() overallLevel: number;
+  @Input() strength: number;
+  @Input() dexterity: number;
+  @Input() constitution: number;
+  @Input() intelligence: number;
+  @Input() wisdom: number;
+  @Input() charisma: number;
+
+  proficiencyBonus: number;
+
+  setStatBlock() {
+    let baseStat = this.characterStats.baseStats;
+    baseStat.strength = this.strength;
+    baseStat.dexterity = this.dexterity;
+    baseStat.constitution = this.constitution;
+    baseStat.intelligence = this.intelligence;
+    baseStat.wisdom = this.wisdom;
+    baseStat.charisma = this.charisma;
+  }
+
+  setModifiedStats() {
+    let baseModifier = this.characterStats.baseModifier;
+    baseModifier.strength = this.modifierService.setModifier(this.strength);
+    baseModifier.dexterity = this.modifierService.setModifier(this.dexterity);
+    baseModifier.constitution = this.modifierService.setModifier(
+      this.constitution
+    );
+    baseModifier.intelligence = this.modifierService.setModifier(
+      this.intelligence
+    );
+    baseModifier.wisdom = this.modifierService.setModifier(this.wisdom);
+    baseModifier.charisma = this.modifierService.setModifier(this.charisma);
+  }
+
+  setProficiency() {
+    this.proficiencyBonus = 1 + Math.ceil(this.overallLevel / 4);
+  }
+
+  setSavingThrows() {
+    let classString = this.characterClass[0];
+    //Pseudocode
+    // string = characterClass.savingProficiencies SWITCH?
+    // switch if match then proficient = true
+    //do for both
+  }
 }
